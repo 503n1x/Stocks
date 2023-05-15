c5e5c19f23ea2271dbe67b9fd3dbdf2ce7f9bcbb
Vit Ink <vitaliyguru@gmail.com>
2023-05-15 19:44:45 +0300

    Initial Commit

---
+++ Stocks/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  Stocks
+//
+//  Created by Vit Gur on 15.05.2023.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
